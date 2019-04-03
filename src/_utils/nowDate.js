40494a81cc15dbe9cb3d69c75c53ff2f145fef9d
lluuii <u0859753@gmail.com>
2019-04-03 17:31:01 +0300

    add date to session in details page

--- src/_utils/nowDate.js
+++ src/_utils/nowDate.js
@@ -1,7 +1,9 @@
 import moment from 'moment';
-import 'moment-timezone';
+// import 'moment-timezone';
 import 'moment/locale/ru';
 
 export const nowDate = new Date( 2019, 2, 20, 10, 0).toJSON(); // сегодняшняя дата
+// export const nowDate = moment();  // сегодняшняя дата
 export const nowDay  = moment(nowDate).format('dddd' );
-export const nowTime = moment(nowDate).format('LT');
+export const nowTime = moment(nowDate).format('HH:mm');
+// console.log('nowTime', nowTime);
--- src/components/sessions.js
+++ src/components/sessions.js
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { nowDay } from "../_utils/nowDate";
-import { dynamicSort } from "../_utils/dynamicSort";
+import {nowDay} from "../_utils/nowDate";
+import {dynamicSort} from "../_utils/dynamicSort";
 
 import SessionTimes from './sessionTimes';
 import moment from 'moment';
 
 class Sessions extends Component {
     render() {
-        const { session } = this.props;
+        const {session} = this.props;
         const resDays = [];
 
-        session && session.map( session => {
-            const dateDay = moment(session.date).format('dddd' );
-            if ( resDays.indexOf( dateDay ) === -1) resDays.push(dateDay);
+        session && session.map(session => {
+            const dateDay = moment(session.date).format('dddd, DD MMMM YYYY');
+            if (resDays.indexOf(dateDay) === -1) resDays.push(dateDay);
             return resDays
         });
         // console.log('resDays', resDays);
-        const dayList = resDays.map(( day, index ) => {
-            return <li className="block-session__li" key={ index }>
-                <span className="block-session__day"> { day } </span>
+        const dayList = resDays.map((day, index) => {
+            return <li className="block-session__li" key={index}>
+                <span className="block-session__day"> {day} </span>
                 <span className="block-session__times">
-                             {
-                                 session.sort(dynamicSort("date")).map( (session, index) => {
-
-                                     const dateDay = moment(session.date).format('dddd' );
-                                     const dateTime = moment(session.date).format('LT' );
-                                     return (
-                                         dateDay >= nowDay && dateDay === day && <SessionTimes time={ dateTime } key={index} session = {session} />
-                                     )
-                                 })
-                             }
-                         </span>
+                    {
+                        session.sort(dynamicSort("date")).map((session, index) => {
+                            const dateDay = moment(session.date).format('dddd');
+                            const dateTime = moment(session.date).format('LT');
+                            return (
+                                dateDay >= nowDay && dateDay === day.split(',')[0] &&
+                                <SessionTimes time={dateTime} key={index} session={session}/>
+                            )
+                        })
+                    }
+                </span>
             </li>
         });
+
         return (
             <div className='block-session'>
-                { dayList }
+                {dayList}
             </div>
         );
     }
 }
+
 export default Sessions;
